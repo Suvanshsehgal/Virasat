c262e47c0753e670fe2f7fb37b789f02b233a279
Suvanshsehgal <suvanshsehgal@gmail.com>
2026-05-25 17:49:11 +0530

    feat : made a central widgets and screen  dart file

---
+++ virasat/lib/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'splash_screen.dart';
+export 'onboarding_screen.dart';
+export 'main_shell.dart';
+export 'tab_screens.dart';
---
+++ virasat/lib/widgets/widgets.dart
@@ -0,0 +1,4 @@
+export 'ashoka_chakra.dart';
+export 'linen_background.dart';
+export 'onboarding_page.dart';
+export 'virasat_bottom_nav.dart';
